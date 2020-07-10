93101d0fad9b6045c7f2f7d798d7f2b6dd34d13a
Maliki <39995350+AkbarMaliki@users.noreply.github.com>
2020-07-10 10:47:18 +0800

    Create lembur_generate.vue

---
+++ src/pages/lembur_generate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+<script>
+import axios from 'axios'
+import firebase from 'firebase'
+let db = firebase.firestore()
+export default{
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  },
+  mounted() {
+    
+  },
+}
+</script>
